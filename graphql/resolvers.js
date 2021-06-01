f5f9f74b1ac57893feeb7386fb4d6c4d4b0505f7
Qifeng Sun <troy@MacBook-Pro.local>
2021-06-01 18:13:24 -0400

    Login and Generating JWT finished

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,18 +1,86 @@
 const { User } = require('../models')
 const bcrypt = require('bcryptjs')
-const { UserInputError } = require('apollo-server')
+const { UserInputError, AuthenticationError } = require('apollo-server')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../config/env.json')
+const { Op } = require('sequelize')
 
 module.exports = {
   Query: {
-    getUsers: async () => {
+    getUsers: async (_, __, context) => {
+      let user
       try {
-        // find all users from the database
-        const users = await User.findAll()
+        // Get the token from the headers and verify it
+        if (context.req && context.req.headers.authorization) {
+          const token = context.req.headers.authorization.split('Bearer ')[1]
+          jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+            if (err) {
+              throw new AuthenticationError('Unauthenticated')
+            }
+            user = decodedToken
+          })
+        }
+
+        // find all users from the database except ourselves
+        const users = await User.findAll({
+          where: { username: { [Op.ne]: user.username } },
+        })
         return users
       } catch (error) {
         console.log(error)
+        throw error
       }
-      return users
+    },
+
+    login: async (_, args) => {
+      const { username, password } = args
+      let errors = {}
+
+      try {
+        // Check if the name and password are not empty
+        if (username.trim() === '')
+          errors.username = 'username must not be emtpy'
+        if (password === '') errors.password = 'password must not be emtpy'
+
+        if (Object.keys(errors).length > 0) {
+          throw new UserInputError('Bad input', { errors })
+        }
+
+        // Find the user in the database
+        const user = await User.findOne({
+          where: { username },
+        })
+
+        if (Object.keys(errors).length > 0) {
+          throw new UserInputError('User not found', { errors })
+        }
+
+        // Decrypt the password and match the user.password
+        const correctPassword = await bcrypt.compare(password, user.password)
+        if (!correctPassword) {
+          errors.password = 'Password is incorrect'
+          throw new AuthenticationError('Password is incorrect', { errors })
+        }
+
+        // Create token
+        const token = jwt.sign(
+          {
+            username,
+          },
+          JWT_SECRET,
+          { expiresIn: 60 * 60 }
+        )
+
+        // Modify the returned user so that the createdAt will be a string
+        return {
+          ...user.toJSON(),
+          createdAt: user.createdAt.toISOString(),
+          token,
+        }
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
     },
   },
   Mutation: {
@@ -31,13 +99,6 @@ module.exports = {
 
         if (password != confirmPassword)
           errors.confirmPassword = 'password must matched'
-
-        //TODO: check if username / email exists
-        // const userByUsername = await User.findOne({ where: { username } })
-        // const userByEmail = await User.findOne({ where: { email } })
-
-        // if (userByUsername) errors.username = 'Username is taken'
-        // if (userByEmail) errors.email = 'Email is taken'
 
         if (Object.keys(errors).length > 0) {
           throw errors
